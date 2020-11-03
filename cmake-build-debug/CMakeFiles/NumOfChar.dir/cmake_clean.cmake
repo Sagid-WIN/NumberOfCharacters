1c6e16d006b2fe0f38cbf8cf9d0a56f002a623a2
Sagid <Irkuev666@gmail.com>
2020-11-03 21:50:26 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/NumOfChar.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NumOfChar.dir/main.c.obj"
+  "NumOfChar.exe"
+  "NumOfChar.exe.manifest"
+  "NumOfChar.pdb"
+  "libNumOfChar.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NumOfChar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
